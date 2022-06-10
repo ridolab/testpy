5f548e81e67a8545af8937187f42a8d37c5bf3b7
Marco Valente <marco.valente@sky.uk>
2022-06-10 03:37:12 +0100

    Beautified the game a bit

--- thegame.py
+++ thegame.py
@@ -1,56 +1,87 @@
 # The fantastic game of Carta, Forbeson, Piera
 
+from dis import Instruction
 from random import randint
 from time import sleep
+import blessed
+
+term = blessed.Terminal()
+
+location_x = int(term.width/2) - 20
+location_y = int(term.height/2) - 20
 
 options = ["P", "C", "F", "X"]
 
 message = {
-  "tie": "Pari! Non te ghe vinto!",
-  "won": "Te ghe vinto!",
-  "lost": "Brao mona, te ghe perso!",
-  "end": "Zugo finio... \n "
+  "tie": term.move_x(location_x) + "Pari, se rifá!",
+  "won": term.move_x(location_x) + "Te ghe vinto!",
+  "lost": term.move_x(location_x) + "Brao mona, te ghe perso!",
+  "end": term.move_x(location_x) + "Zugo finio..." + term.clear() + term.home()
 }
 
-def decide_winner(user_choice, computer_choice):
+counter = {"User": 0, "Computer": 0}
 
-  print ("La scelta del Computer: %s" % (computer_choice))
-  print()
-  
-  if (user_choice!=options[0] and user_choice!=options[1] and user_choice!=options[2] and user_choice!=options[3]):
-    print("Te ghe sbaglia scelta!")  
-  elif (user_choice==computer_choice):
-    print (message["tie"])
-  elif (user_choice==options[0] and computer_choice==options[2]):
-    print (message["won"])
-  elif (user_choice==options[1] and computer_choice==options[0]):
-    print (message["won"])
-  elif (user_choice==options[2] and computer_choice==options[1]):
-    print (message["won"])
+instructions = [
+  "- - - - - - - - - - -",
+  "",
+  "Premi C per Carta... F per Forbeson... P per Piera...",
+  "",
+  "Premi X per uscire",
+  "",
+  "- - - - - - - - - - -"
+]
+
+
+def decide_winner(user_choice, computer_choice):
+  global counter
+
+  if (user_choice in options):
+    print(term.move_x(location_x) + "La scelta del Computer: %s" % (computer_choice), end="")
+    print()
+    if (user_choice==computer_choice):
+      print (message["tie"])
+    elif (user_choice==options[0] and computer_choice==options[2]):
+      print (message["won"])
+      counter["User"] += 1
+    elif (user_choice==options[1] and computer_choice==options[0]):
+      print (message["won"])
+      counter["User"] += 1
+    elif (user_choice==options[2] and computer_choice==options[1]):
+      print (message["won"])
+      counter["User"] += 1
+    else:
+      print (message["lost"])
+      counter["Computer"] += 1
   else:
-    print (message["lost"])
+    print(term.move_x(location_x) + "Te ghe sbagliá scelta, riprova!")
 
 def play_RPS():
+  global counter
   play = True
+  # clear the screen
+  print(term.home + term.cadetblue1_on_bisque4 + term.clear)
   print ()
   print ("Te ste zugando al fantastico zugo: Carta, Forbeson, Piera \n")
   print ("Caricamento in corso...")
-  sleep(5)
-  print ("- - - - - - - - - -")
-  print ("Premi C per Carta... F per Forbeson... P per Piera...")
-  print ()
-  print ("Premi X per uscire")
-  print ("- - - - - - - - - -")  
-  while play :   
-    user_choice = input("Ti sa dito: Carta, Forbeson, Piera: ")
-    print ()
-    user_choice = user_choice.upper()
-    if user_choice == options[3]:
+  sleep(1)
+  while play :
+    # clear the screen
+    print(term.home + term.cadetblue1_on_bisque4 + term.clear)
+    print (term.move_x(location_x) + "TI  " + str(counter["User"]), "-", str(counter["Computer"]) + "  EL COMPUTER")
+    print(term.move_y(location_y))
+    for line in instructions:
+      print(term.move_x(location_x) + line)
+    print(term.move_x(location_x) + "Ti sa dito: Carta, Forbeson, Piera: ")
+    user_choice = ''
+    with term.cbreak():
+      user_choice = term.inkey(timeout=3)
+      user_choice = user_choice.upper()
+      if user_choice == options[3]:
         play = False
         print (message["end"])
         break
-    computer_choice = options[randint(0, 2)]
-    decide_winner(user_choice, computer_choice)
-    print("- - - - - - - - - -")
-    sleep (3)
+      computer_choice = options[randint(0, 2)]
+      decide_winner(user_choice, computer_choice)
+      sleep (2)
+
 play_RPS()
